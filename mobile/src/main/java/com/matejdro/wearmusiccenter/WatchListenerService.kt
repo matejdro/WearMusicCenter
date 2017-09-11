e7a07c1e6ba50d76803eb6d584251e8751fdaa4d
Matej Drobnič <matejdro@gmail.com>
2017-09-11 17:16:51 +0200

    Do not attempt to restart service when watch closed event is received.

--- mobile/src/main/java/com/matejdro/wearmusiccenter/WatchListenerService.kt
+++ mobile/src/main/java/com/matejdro/wearmusiccenter/WatchListenerService.kt
@@ -3,11 +3,12 @@ package com.matejdro.wearmusiccenter
 import android.content.Intent
 import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
+import com.matejdro.wearmusiccenter.common.CommPaths
 import com.matejdro.wearmusiccenter.music.MusicService
 
 class WatchListenerService : WearableListenerService() {
     override fun onMessageReceived(event: MessageEvent?) {
-        if (event == null) {
+        if (event == null || event.path == CommPaths.MESSAGE_WATCH_CLOSED) {
             return
         }
 
