349f058ce21ca15f8231e42ee97a3a80494207a8
Matej Drobnič <matejdro@gmail.com>
2017-10-26 22:28:47 +0200

    Disable swipe-to-close when action menu is open.

---
+++ wear/src/main/java/com/matejdro/wearmusiccenter/watch/view/NoSwipeCloseDrawerView.kt
@@ -0,0 +1,17 @@
+package com.matejdro.wearmusiccenter.watch.view
+
+import android.content.Context
+import android.support.wear.widget.drawer.WearableDrawerView
+import android.util.AttributeSet
+
+/**
+ * Drawer view that will disable swipe-to-close function when open
+ */
+class NoSwipeCloseDrawerView : WearableDrawerView {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    override fun canScrollHorizontally(direction: Int): Boolean = true
+}
