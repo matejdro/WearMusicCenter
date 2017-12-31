6bb45af8e63ca5e1468bd977f2b2869d857e98e1
Matej Drobnič <matejdro@gmail.com>
2017-12-31 08:22:41 +0100

    If music app does not start playback, start their UI first.

--- mobile/src/main/java/com/matejdro/wearmusiccenter/actions/appplay/AppPlayAction.kt
+++ mobile/src/main/java/com/matejdro/wearmusiccenter/actions/appplay/AppPlayAction.kt
@@ -12,20 +12,24 @@ import com.matejdro.wearmusiccenter.R
 import com.matejdro.wearmusiccenter.actions.PhoneAction
 import com.matejdro.wearmusiccenter.actions.SelectableAction
 import com.matejdro.wearmusiccenter.music.MusicService
+import com.matejdro.wearmusiccenter.music.isPlaying
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 
 class AppPlayAction : SelectableAction {
     companion object {
         const val KEY_PACKAGE_NAME = "PACKAGE_NAME"
         const val KEY_CLASS_NAME = "COMPONENT_NAME"
-
     }
 
-    private lateinit var receiverComponent : ComponentName
+    private lateinit var receiverComponent: ComponentName
 
-    constructor(context : Context, receiverComponent : ComponentName) : super(context) {
+    constructor(context: Context, receiverComponent: ComponentName) : super(context) {
         this.receiverComponent = receiverComponent
     }
-    constructor(context : Context, bundle: PersistableBundle) : super(context, bundle) {
+
+    constructor(context: Context, bundle: PersistableBundle) : super(context, bundle) {
         this.receiverComponent = ComponentName(
                 bundle.getString(KEY_PACKAGE_NAME),
                 bundle.getString(KEY_CLASS_NAME)
@@ -35,17 +39,17 @@ class AppPlayAction : SelectableAction {
     private val lazyIcon by lazy {
         try {
             context.packageManager.getApplicationIcon(receiverComponent.packageName)
-        } catch(ignored: PackageManager.NameNotFoundException) {
+        } catch (ignored: PackageManager.NameNotFoundException) {
             context.getDrawable(android.R.drawable.sym_def_app_icon)
         }
     }
 
-    private val lazyName  by lazy {
-        var appName : String
+    private val lazyName by lazy {
+        var appName: String
         try {
             val appInfo = context.packageManager.getApplicationInfo(receiverComponent.packageName, 0)
             appName = context.packageManager.getApplicationLabel(appInfo).toString()
-        } catch(ignored: PackageManager.NameNotFoundException) {
+        } catch (ignored: PackageManager.NameNotFoundException) {
             context.getDrawable(android.R.drawable.sym_def_app_icon)
             appName = context.getString(R.string.uninstalled_app)
         }
@@ -66,12 +70,44 @@ class AppPlayAction : SelectableAction {
             }
         }
 
-        val eventTime = System.currentTimeMillis()
+        val androidContext = context
 
+        launch(UI) {
+            dispatchUpDownEvents(pickedDeviceID)
+            delay(500)
+
+            if (service.currentMediaController?.packageName == receiverComponent.packageName &&
+                    service.currentMediaController?.isPlaying() == true) {
+                return@launch
+            }
+
+            // Media is still not playing.
+            // Some players do not handle background starts very well. Start their UI activity first.
+            val launcherActivity = androidContext.packageManager.getLaunchIntentForPackage(receiverComponent.packageName)
+            if (launcherActivity != null) {
+                androidContext.startActivity(launcherActivity)
+                delay(500)
+                dispatchUpDownEvents(pickedDeviceID)
+                delay(500)
+            }
+
+            if (service.currentMediaController?.packageName == receiverComponent.packageName &&
+                    service.currentMediaController?.isPlaying() == true) {
+                return@launch
+            }
+
+            // Media is still not playing.
+            // Maybe UI activity took some time to restart?
+            // Retry sending play command one last time after 3 second delay
+            delay(3000)
+            dispatchUpDownEvents(pickedDeviceID)
+        }
+    }
+
+    private fun dispatchUpDownEvents(pickedDeviceID: Int, eventTime: Long = System.currentTimeMillis()) {
         dispatchKeyEvent(KeyEvent(0, eventTime, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PLAY, 0, 0, pickedDeviceID, 0))
         dispatchKeyEvent(KeyEvent(0, eventTime, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PLAY, 0, 0, pickedDeviceID, 0))
     }
-
 
     private fun dispatchKeyEvent(keyEvent: KeyEvent) {
         val intent = Intent(Intent.ACTION_MEDIA_BUTTON)
@@ -92,7 +128,7 @@ class AppPlayAction : SelectableAction {
     override val defaultIcon: Drawable
         get() = lazyIcon
 
-    override fun isEqualToAction(other : PhoneAction) : Boolean {
+    override fun isEqualToAction(other: PhoneAction): Boolean {
         other as AppPlayAction
         return super.isEqualToAction(other) && this.receiverComponent == other.receiverComponent
     }
