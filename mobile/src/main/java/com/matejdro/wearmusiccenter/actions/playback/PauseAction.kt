2c280232b846dbfe5007a6a152095ee6ab6ff079
Matej Drobnic <matejdro@gmail.com>
2021-09-14 10:24:27 +0200

    fix: play/pause not working for some media apps

--- mobile/src/main/java/com/matejdro/wearmusiccenter/actions/playback/PauseAction.kt
+++ mobile/src/main/java/com/matejdro/wearmusiccenter/actions/playback/PauseAction.kt
@@ -3,6 +3,7 @@ package com.matejdro.wearmusiccenter.actions.playback
 import android.content.Context
 import android.graphics.drawable.Drawable
 import android.os.PersistableBundle
+import android.view.KeyEvent
 import com.matejdro.wearmusiccenter.R
 import com.matejdro.wearmusiccenter.actions.SelectableAction
 import com.matejdro.wearmusiccenter.music.MusicService
@@ -12,7 +13,10 @@ class PauseAction : SelectableAction {
     constructor(context : Context, bundle: PersistableBundle) : super(context, bundle)
 
     override fun execute(service: MusicService) {
-        service.currentMediaController?.transportControls?.pause()
+        service.currentMediaController?.let {
+            it.dispatchMediaButtonEvent(KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_MEDIA_PAUSE))
+            it.dispatchMediaButtonEvent(KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_PAUSE))
+        }
     }
 
     override fun retrieveTitle(): String = context.getString(R.string.action_pause)
